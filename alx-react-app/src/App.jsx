ebf999c8814ef440e0c5c897e3699ba0a318e2ec
Martin Odongo <martinwere14@gmail.com>
2024-10-30 19:05:44 +0300

    react set up

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import WelcomeMessage from './WelcomeMessage'
+import WelcomeMessage from './components/WelcomeMessage'
 import Header from './Header';
 import MainContent from './MainContent'
 import Footer from './Footer';
