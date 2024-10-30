925a837b59475471ff41763aadd90f868f257d63
Martin Odongo <martinwere14@gmail.com>
2024-10-30 18:57:31 +0300

    react

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -5,14 +5,12 @@ import Header from './components/Header';
 import MainContent from './components/MainContent'
 import Footer from './components/Footer';
 function App(){
-  const [count, setCount] = useState(0)
 
   return (
     <>
      <Header/>
      <MainContent/>
      <Footer/>
-    <WelcomeMessage />
     </>
   )
 }
