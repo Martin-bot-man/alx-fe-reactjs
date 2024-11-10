2f1b0150741104cf12c5b2e448d35a8dbd51744d
Martin Odongo <martinwere14@gmail.com>
2024-11-10 21:56:25 +0300

    added MyContect component

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header';
+import MainContent from './components/MainContent'
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile'
+import MyContext from './components/UserContext';
+function App(){
+  const [ theme, setTheme ] = useState('dark');
+  const [ count, setCount ] = useState(0);
+
+  return (
+  <>
+    <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
+     <Header />
+     <MainContent />
+     <Footer />
+  </>
+  )
+}
+
+export default App
