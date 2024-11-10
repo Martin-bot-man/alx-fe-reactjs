4db210c55bbe7a212ad3f0fd1f0e139a0a38cdce
Martin Odongo <martinwere14@gmail.com>
2024-11-10 12:17:59 +0300

    added the Counter component

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent'
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter';
 function App(){
   const [ theme, setTheme ] = useState('dark');
   const [ count, setCount ] = useState(0);
@@ -14,6 +15,7 @@ function App(){
     <UserProfile name="Alice" age="25 " bio="Loves playing football"/>
      <Header />
      <MainContent />
+     <Counter/>
      <Footer />
   </>
   )
