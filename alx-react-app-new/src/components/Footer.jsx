d9747c6c130bd05de06614dc292392eb2474d6ce
Martin Odongo <martinwere14@gmail.com>
2024-11-10 11:11:09 +0300

    styled the footer and mainContent components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+ const Footer = ()=>{
+    return(
+        <footer>
+           <p style= {{color:'red', textDecoration:'underline' }}>© 2023 City Lovers</p>
+        </footer>
+    )
+}
+export default Footer
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+const MainContent = ()=>{
+    return(
+        <main>
+            <p style ={{color:'rebeccapurple', backgroundColor:'red'}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    )
+}
+export default MainContent;
