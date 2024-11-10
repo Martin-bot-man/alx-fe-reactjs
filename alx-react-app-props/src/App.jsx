1d44bcead68eb4b31554185e9154adfafb163326
Martin Odongo <martinwere14@gmail.com>
2024-11-10 23:15:15 +0300

    learning how to use UserContext

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -5,17 +5,20 @@ import Header from './components/Header';
 import MainContent from './components/MainContent'
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile'
-import MyContext from './components/UserContext';
-function App(){
+import UserContext from './components/UserContext';
+import ProfilePage from './components/ProfilePage';
+
+const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+
   const [ theme, setTheme ] = useState('dark');
   const [ count, setCount ] = useState(0);
 
   return (
   <>
-    <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
-     <Header />
-     <MainContent />
-     <Footer />
+  <UserContext.Provider value={userData}>
+    <ProfilePage />
+    </UserContext.Provider>.
   </>
   )
 }
