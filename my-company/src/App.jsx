5804fe00d6af799e6e61e13e97c6d90cabc2ac0a
Martin Odongo <martinwere14@gmail.com>
2024-11-11 15:10:18 +0300

    building a 4 page simple company website

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import React from 'react'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import Services from './pages/Services'
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    
+      <BrowserRouter>
+      <Routes>
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="home" element={<Home />} />
+          <Route path="services" element={<Services />} />
+
+      </Routes>
+      </BrowserRouter>
+    
   )
 }
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 export default App
