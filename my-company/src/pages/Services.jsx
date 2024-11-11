0e92261d1e0c333d1bc6afa6210835f8e1e490a2
Martin Odongo <martinwere14@gmail.com>
2024-11-11 16:37:33 +0300

    built the page navbarcomponent and made it appear in all pages

--- my-company/src/pages/Services.jsx
+++ my-company/src/pages/Services.jsx
@@ -3,7 +3,7 @@ function Services() {
     return (
       <div style={{ padding: '20px' }}>
         <h1>Our Services</h1>
-        <ul>
+        <ul style={{ListStyletype: 'none'}}>
           <li>Technology Consulting</li>
           <li>Market Analysis</li>
           <li>Product Development</li>
