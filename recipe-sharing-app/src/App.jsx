8b368f0bb0cc3fe1479afcf67a764ec8216d4f4b
Martin Odongo <martinwere14@gmail.com>
2024-11-16 23:05:24 +0300

    added the recipelist and the addrecipe form to the app.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import AddRecipeForm from '/.components/AddRecipeForm'
+import Recipelist from '/.components/RecipeList'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
