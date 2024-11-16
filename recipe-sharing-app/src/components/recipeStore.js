58861fe4459a4a734d21fdc9023acd358313656f
Martin Odongo <martinwere14@gmail.com>
2024-11-17 02:27:32 +0300

    used zustand for state management

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -4,5 +4,8 @@ const useRecipeStore = create(set=>({
     recipes:[],
     addRecipe: (newRecipe)=> set(state=>({recipes: [...state.recipes, newRecipe]})),
     setRecipes: (recipes)=> set({recipes})
+    updateRecipe: (newUpdates)=> set({newupdates})
+    deleteRecipe: (deleteRecipe)=> set({})
+
 }));
 export default useRecipeStore;
