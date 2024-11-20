b0d517e61742ee2d82ab3c0e9d340adec0827a35
Martin Odongo <martinwere14@gmail.com>
2024-11-21 00:52:29 +0300

    fixed bug in the route and routes

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,12 +1,33 @@
 //RecipeList component
 import React from 'react'
   import { useRecipeStore } from './zustand';
+  import React,{useState, useEffect} from 'react';
 
   const RecipeList = () => {
     const recipes = useRecipeStore(state => state.recipes);
+    const [filters, setFilters] = useState({});
+    const {
+      filteredRecipes,
+      searchAndFilterRecipes// assuming action exists in your store
+      
+    }=useRecipeStore();
+
+    const handleSearch = (term)=> {
+      setSearchTerm(term);
+    };
+    const handleFilterChange = (newFilters)=> {
+      setFilters({...filters,...newFilters});
+    };
+    useEffect(()=>{
+      searchAndFilterRecipes(searchTerm, filters);
+
+    },[searchTerm, filters, searchandFilterRecipes]);
 
     return (
       <div>
+        <SearchBar onSearch={handleSearch}/>
+        <FilterOptions onFilterChange={handleFilterChange}/>
+        {/* display recipes from the zustand store*/}
         {recipes.map(recipe => (
           <div key={recipe.id}>
             <h3>{recipe.title}</h3>
--- recipe-sharing-app/src/useRecipeStore.js
+++ recipe-sharing-app/src/useRecipeStore.js
@@ -2,6 +2,8 @@ import create from 'zustand'
 
 const useRecipeStore = create(set => ({
   recipes: [], 
+  searchTerm:'',
+
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes }), 
   deleteRecipe:(recipeId)=>set(state => ({
@@ -11,7 +13,25 @@ const useRecipeStore = create(set => ({
     recipes:state.recipes.map(recipe=>
       recipe.id === updateRecipe.id ? updateRecipe :recipe
     )
-  }))
+  })),
+  setSearchTerm: (term)=> set({searchTerm: term}),
+
+  getfilteredRecipes: ()=> set(state => ({
+    filteredRecipes :state.recipes.filter(recipe => recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+  )
+  })),
+  const SearchBar =()=>{
+    const setSearchTerm = useRecipeStore(state => state.setSearchTerm);
+
+    return(
+      <input
+      type="text"
+      placeholder= "Search recipes..."
+      onChange = {(e) => setSearchTerm(e.target.value)}
+      />
+    )
+   
+  }
 }));
 
 export default useRecipeStore
