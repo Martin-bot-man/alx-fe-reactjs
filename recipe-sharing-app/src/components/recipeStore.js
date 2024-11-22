5201a0ef4401ac193e1e1d7c8a906e91b040dcb3
Martin Odongo <martinwere14@gmail.com>
2024-11-22 20:42:57 +0300

    created the Recipe list component

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,9 +3,9 @@ import create from 'zustand';
 const useRecipeStore = create(set=>({
     recipes:[],
     addRecipe: (newRecipe)=> set(state=>({recipes: [...state.recipes, newRecipe]})),
-    setRecipes: (recipes)=> set({recipes})
-    updateRecipe: (newUpdates)=> set({newupdates})
-    deleteRecipe: (deleteRecipe)=> set({})
+    setRecipes: (recipes)=> set({recipes}),
+    updateRecipe: (newUpdates)=> set({newUpdates}),
+    deleteRecipe: (deleteRecipe)=> set({deleteRecipe})
 
 }));
 export default useRecipeStore;
--- recipe-sharing-app/src/useRecipeStore.js
+++ recipe-sharing-app/src/useRecipeStore.js
@@ -20,18 +20,7 @@ const useRecipeStore = create(set => ({
     filteredRecipes :state.recipes.filter(recipe => recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
   )
   })),
-  const SearchBar =()=>{
-    const setSearchTerm = useRecipeStore(state => state.setSearchTerm);
-
-    return(
-      <input
-      type="text"
-      placeholder= "Search recipes..."
-      onChange = {(e) => setSearchTerm(e.target.value)}
-      />
-    )
-   
-  }
+  
 }));
 
 export default useRecipeStore
