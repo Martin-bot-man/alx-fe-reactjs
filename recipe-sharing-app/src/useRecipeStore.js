389701a62719f96f19147ebe7b57d83bb355ae56
Martin Odongo <martinwere14@gmail.com>
2024-11-23 00:42:42 +0300

    added RecipeForm component to app.jsx

--- recipe-sharing-app/src/useRecipeStore.js
+++ recipe-sharing-app/src/useRecipeStore.js
@@ -3,7 +3,7 @@ import create from 'zustand'
 const useRecipeStore = create(set => ({
   recipes: [], 
   searchTerm:'',
-
+favorites:[],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes }), 
   deleteRecipe:(recipeId)=>set(state => ({
@@ -14,13 +14,35 @@ const useRecipeStore = create(set => ({
       recipe.id === updateRecipe.id ? updateRecipe :recipe
     )
   })),
+  addFavorite: (recipeId)=> set(state=>({favorites:[...state.favorites, recipeId]})),
+  removeFavorite: (recipeId)=> set(state=> ({
+    favorites:state.favorites.filter(id=>id != recipeId)
+  })),
+  recommendations:[],
+  generateRecommendations:()=> set(state=>{
+    //mock implementation
+    const recommended = state.recipes.filter(recipe=> 
+      state.favorites.includes(recipe.id)&& Math.random()> 0.5
+    );
+  })
   setSearchTerm: (term)=> set({searchTerm: term}),
 
   getfilteredRecipes: ()=> set(state => ({
     filteredRecipes :state.recipes.filter(recipe => recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
-  )
+  ),
   })),
-  
+  const SearchBar =()=>{
+    const setSearchTerm = useRecipeStore(state => state.setSearchTerm);
+
+    return(
+      <input
+      type="text"
+      placeholder= "Search recipes..."
+      onChange = {(e) => setSearchTerm(e.target.value)}
+      />
+    )
+   
+  }
 }));
 
 export default useRecipeStore
