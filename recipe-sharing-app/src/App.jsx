cf65a6e39e0e22e0e7a3ebca8ae9c90b15bc9deb
Martin Odongo <martinwere14@gmail.com>
2024-11-22 23:28:18 +0300

    added RecipeForm component to app.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import RecipeDetails from './components/RecipeDetails';
+import AddRecipeForm from './components/AddRecipeForm.jsx';
 import route from './routes.js';
 
 const App = ()=> {
