babd564fb1d83aa227dd4e8a8f30c829d59d8e10
Martin Odongo <martinwere14@gmail.com>
2024-11-23 03:05:31 +0300

    added favourites and a recommendation system for the recipe

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -4,16 +4,15 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import RecipeDetails from './components/RecipeDetails';
 import AddRecipeForm from './components/AddRecipeForm.jsx';
 import route from './routes.js';
+import FavoritesList from './components/FavoritesList.jsx';
+import RecommendationList from './components/RecommendationsList.jsx';
 
 const App = ()=> {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path ="/" element = {<App />} />
-                <Route path = "/recipes/ :recipeId" element= {<RecipeDetails />} />
-            </Routes>
-
-        </BrowserRouter>
+     <>
+        <FavoritesList />
+        <RecommendationList />
+     </>
     )
 }
 
--- recipe-sharing-app/src/useRecipeStore.js
+++ recipe-sharing-app/src/useRecipeStore.js
@@ -3,7 +3,8 @@ import create from 'zustand'
 const useRecipeStore = create(set => ({
   recipes: [], 
   searchTerm:'',
-favorites:[],
+  favorites:[],
+  recommendations:[],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes }), 
   deleteRecipe:(recipeId)=>set(state => ({
@@ -18,12 +19,13 @@ favorites:[],
   removeFavorite: (recipeId)=> set(state=> ({
     favorites:state.favorites.filter(id=>id != recipeId)
   })),
-  recommendations:[],
   generateRecommendations:()=> set(state=>{
     //mock implementation
-    const Recommended = state.recipes.filter(recipe=> 
+    const recommended = state.recipes.filter(recipe=> 
       state.favorites.includes(recipe.id)&& Math.random()> 0.5
     );
+
+    return { recommendations: recommended };
   }),
   setSearchTerm: (term)=> set({searchTerm: term}),
 
@@ -31,18 +33,7 @@ favorites:[],
     filteredRecipes :state.recipes.filter(recipe => recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
   ),
   })),
-  const SearchBar =()=>{
-    const setSearchTerm = useRecipeStore(state => state.setSearchTerm);
-
-    return(
-      <input
-      type="text"
-      placeholder= "Search recipes..."
-      onChange = {(e) => setSearchTerm(e.target.value)}
-      />
-    )
-   
-  }
+ 
 }));
 
 export default useRecipeStore
