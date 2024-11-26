3ccf5b4bbd662a8343a58868e5a903a6f4bc592b
Martin Odongo <martinwere14@gmail.com>
2024-11-26 23:53:35 +0300

    feat:made the registration form component

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -6,13 +6,24 @@ const RegistrationForm = () => {
   const[username, setUserName] = useState('');
   const[email, setEmail] = useState('');
   const[password, setPassword] = useState('')
+  const[errors, setErrors] = useState({});
 
   const handleSubmit = (event)=>{
     event.preventDefault();
-if(!username|| !email || !password){
-    alert('Please fill in all fields');
+
+    const newErrors = {};
+    if(!username) newErrors.username = 'Username is required';
+    if(!email) newErrors.email = 'Email is required';
+    if(!password) newErrors.password = 'Password is required';
+
+    setErrors(newErrors);
+
+    if(Object.keys(newErrors).length > 0){
+        return;
+    }
+
+
 }
-  }
     return (
         <form onSubmit={handleSubmit}>
             <div>
@@ -23,6 +34,7 @@ if(!username|| !email || !password){
                 value={username}
                 onChange={(e) => setUserName(e.target.value)}
                 required/>
+                {errors.username && <span className = "error">{errors.username}</span>}
             </div>
             <div>
                 <label htmlFor="email">Email:</label>
@@ -33,6 +45,7 @@ if(!username|| !email || !password){
                 onChange={(e)=> setEmail(e.target.value)}
                 required
                 />
+                {errors.email && <span className ="error">{errors.email}</span>}
             </div>
             <div>
                 <label htmlFor="password">Password:</label>
@@ -42,8 +55,8 @@ if(!username|| !email || !password){
                 value={password}
                 onChange= {(e)=>setPassword(e.target.value)}
                 required
-
                 />
+                {errors.password && <span className="error">{errors.password}</span>}
             </div>
             <button type="submit">Register</button>
 
