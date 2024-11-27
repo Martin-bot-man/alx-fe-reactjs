bd267500109d2d934e8beb6697c97290e9ee5797
Martin Odongo <martinwere14@gmail.com>
2024-11-27 04:12:02 +0300

    replaced input field with label in formikForm.js component

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import {useFormik} from 'formik';
-
+import {useFormik, Field, ErrorMessage} from 'formik';
+import * as Yup from 'yup';
 
 const formikForm = () => {
 
     const formik = useFormik({
         initialValues: {
             email:'',
-            userName:'',
+            name:'',
             password:'',
         },
+        validationSchema:Yup.object({
+            name: Yup.string()
+            .min(2, 'Name must be atleast 2 characters')
+            .required('Required'),
+            email:Yup.string()
+            .email('Invalid email address')
+            .required('Required'),
+            password:Yup.string()
+            .min(8, 'Password is required')
+            .matches(/[0-9]/, 'Password must contain at least one digit')
+            .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
+            .matches(/[A-Z]/, 'Passwoed must contain at least one uppercase letter'),
+            passwordConfirmation :string()
+            .oneOf([Yup.ref('password'), null], 'Passwords must match')
+            .required('Required'),
+        }),
 
         onSubmit: values => {
             alert(JSON.stringify(values, null,2));
@@ -39,8 +55,9 @@ const formikForm = () => {
     })
   return (
     <form onSubmit={formik.handleSubmit}>
-        <label htmkFor="name">Name:</label>
-        <input
+       <div>
+       <label htmkFor="name">Name:</label>
+        <Field
         type='text'
         id='name'
         name='name'
@@ -48,9 +65,14 @@ const formikForm = () => {
         onBlur={formik.handleBlur}
         value={formik.values.name}
         />
+        <ErrorMessage name="name" component="div" className="error"/>
+       </div>
 
+        
+
+    <div>
         <label htmlFor="email">Email:</label>
-        <input
+        <Field
         type='email'
         id='email'
         name='email'
@@ -58,16 +80,24 @@ const formikForm = () => {
         onBlur={formik.handleBlur}
         value={formik.values.email}
         />
-        <label htmlFor="Password">Password:</label>
-        <input 
+                <ErrorMessage name="email" component="div" className="error" />
+
+    </div>
+      <div>
+      <label htmlFor="Password">Password:</label>
+        <Field
         type='Password'
-        id='password'
-        name='password'
+        id='passwordConfirmation'
+        name='passwordConfirmation'
         onChange={formik.handleChange}
         onBlur={formik.handleBlur}
-        value={formik.values.password}
+        value={formik.values.passwordConfirmation}
         
         />
+                <ErrorMessage name="password" component="div" className="error" />
+
+      </div>
+        
         <button type="submit">Submit</button>
 
 
