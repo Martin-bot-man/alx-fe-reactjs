444bedb98d56f12403989c6b3ec1533ef3fe7290
Martin Odongo <martinwere14@gmail.com>
2024-11-29 00:47:06 +0300

    feat:implementing advanced routing

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,22 @@
+import {BrowserRouter as Router, Routes, Link} from 'react-router-dom';
+import './App.css'
+
+function App() {
+
+  return (
+    
+    <Router>
+      <div>
+
+      <Routes>
+
+      </Routes>
+      
+      </div>
+    </Router>
+     
+    
+  )
+}
+
+export default App
