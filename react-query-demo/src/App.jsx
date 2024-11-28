4bd2317f898fba03702a6f335f9d4f82b7dc080c
Martin Odongo <martinwere14@gmail.com>
2024-11-28 14:23:07 +0300

    feat: fetched posts from a public API using react query

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,5 +1,5 @@
 import {QueryClient, QueryClientProvider} from 'react-query';
-import PostsComponent from './component /PostsComponent';
+import PostsComponent from './components/PostsComponent';
 
 
 const queryClient = new QueryClient();
