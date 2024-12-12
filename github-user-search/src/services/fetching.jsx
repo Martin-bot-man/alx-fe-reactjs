41cc0989b35899c69e193d2b8dd83e2b610a7bbd
Martin Odongo <martinwere14@gmail.com>
2024-12-13 00:10:23 +0300

    feat: initiated a fetch function using axios

--- github-user-search/src/services/fetching.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const fetching = () => {
-  return (
-    <div>fetching</div>
-  )
-}
-
-export default fetching
---
+++ github-user-search/src/services/githubService.jsx
@@ -0,0 +1,21 @@
+import axios from 'axios'
+const BASE_URL = "https://api.github.com/users";
+
+const githubService = {
+  fetchUserData:async (username) => {
+  try{
+    const response = await axios.get(`${BASE_URL}/${username} `);
+    return response.data;
+
+  }
+  catch(error){
+    console. error('Error fetching user data:', error);
+    return null;
+
+  };
+  
+  
+},
+}
+
+export default githubService
