13bb435911bbf4da6c6aaeb3c07d48fd2283b957
Martin Odongo <martinwere14@gmail.com>
2024-12-14 12:25:31 +0300

    feat:created error handling logic

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,24 +1,55 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import {useState} from react;
 
 const Search = () => {
-    const[userName, setUserName] = useState('');
+    const[username, setUsername] = useState('');
 
-    const handleSubmit = async (e)=> {e.preventDefault()
+    const[userData, setUserData] = useState(null);
+    const [isLoading, setIsLoading]= useState(false);
+    cosnt[error,setError]= useState(null);
+    const handleSubmit = async (e)=> {e.preventDefault();
+        setIsLoading(true);
+        setError(null);
+        try{
+            const response = await axios.get(`https://api.github.com/users/${username}`);
+            setUserData(response.data);
+        
+        }
+        catch(error){
+            setError('Looks like we cant find the user');
+        }
+        finally{
+            setIsLoading(false);
+        }
        
-    }
+    };
   return (
     <div>
         <form onSubmit={handleSubmit}>
-            <input type ="text" value={userName} onChange={(e)=>setUserName(e.target.value)}/>
-            <div>
-                <button type ="submit">Submit</button>
-            </div>
+            <input 
+            type="text"
+            value={username}
+            onChange={(e)=>setUsername(e.target.value)}/>
 
+            <button type= "submit">Search</button>
         </form>
-
+        {isLoading?(
+            <p>Loading...</p>
+        ):error?(
+            <p>{error}</p>
+        ):userData?(
+            <div>
+                <h2>{userData.login}</h2>
+                <img src={userData.avatar_url} alt={userData.login}/>
+                <P>{userData.bio}</P>
+                <a href={userData.html_url} target="_blank" rel="nooopener noreferrer">View GitHub Profile</a>
+            </div>
+        ):null
+    }
     </div>
   )
+    
+        
 }
 
 export default Search
